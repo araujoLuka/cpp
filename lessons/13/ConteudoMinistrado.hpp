5c0e5da031022c883aecf1608dcead20539684d6
Lucas Araujo (DINF) <lca20@inf.ufpr.br>
2023-09-27 06:47:22 -0300

    added lesson 13

---
+++ lessons/13/ConteudoMinistrado.hpp
@@ -0,0 +1,20 @@
+#ifndef CONTEUDO_MINISTRADO_HPP
+#define CONTEUDO_MINISTRADO_HPP
+
+#include<string>
+
+class ConteudoMinistrado{
+    public:
+        ConteudoMinistrado(const std::string& descricao, const unsigned short cargaHorariaConteudo);
+
+        const std::string& getDescricao() const;
+        unsigned short getCargaHorariaConteudo() const;
+        unsigned int getId() const;
+    private:
+        static unsigned int proxId;
+
+        std::string descricao;
+        unsigned short cargaHorariaConteudo;
+        unsigned int id;
+};
+#endif
